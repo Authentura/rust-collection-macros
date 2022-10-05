775fbec0fa3513ea67399618576b217896c7644c
Samuel Boyden <sam@stboyden.com>
2022-10-05 15:56:09 +0100

    Add generic `set` macro

--- src/sets/mod.rs
+++ src/sets/mod.rs
@@ -1,4 +1,28 @@
 pub mod btreeset;
 pub mod hashset;
+
 pub use btreeset::*;
 pub use hashset::*;
+
+#[macro_export]
+macro_rules! set {
+    {} => {
+        {
+            #[cfg(set-macro-use-hset)]
+            hset!{}
+            #[cfg(set-macro-use-bset)]
+            bset!{}
+        }
+    };
+    { $($value:expr),* $(,)? } => {
+        {
+            let mut set = set!{};
+
+            $(
+                set.insert($value);
+            )*
+
+            set
+        }
+    }
+}
