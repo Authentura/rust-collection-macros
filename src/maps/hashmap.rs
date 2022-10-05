0f9a579910990d275a4ec34d092c45dbc27a2c0d
Samuel Boyden <sam@stboyden.com>
2022-10-05 15:40:16 +0100

    Improve HashMap macro declaration

--- src/maps/hashmap.rs
+++ src/maps/hashmap.rs
@@ -1,40 +1,24 @@
 #[macro_export]
 macro_rules! hmap {
-    {} => {{
-        #[cfg(not(dashmap))]
-        use std::collections::HashMap;
-        #[cfg(dashmap)]
-        use dashmap::DashMap as HashMap;
+    {} => {
+        {
+            #[cfg(not(dashmap))]
+            use std::collections::HashMap;
+            #[cfg(dashmap)]
+            use dashmap::DashMap as HashMap;
 
-        HashMap::new()
-    }};
-    { $( $key:tt => $value:tt ),* } => {{
-        let mut hm = hmap!{};
+            HashMap::new()
+        }
+    };
+    { $($key:expr => $value:expr),* $(,)? } => {
+       {
+           let mut hm = hmap!{};
 
-        $(
-            hm.insert($key, $value);
-        )*
+           $(
+               hm.insert($key, $value);
+           )*
 
-        hm
-    }};
-    { $( $key:tt => $values:expr ),* } => {{
-        let mut hm = hmap!{};
-
-        $(
-            hm.insert($key, $values.collect::<Vec<_>>());
-        )*
-
-        hm
-    }};
-    { $( $range:expr => $value:tt ),* } => {{
-        let mut hm = hmap!{};
-
-        $(
-            ($range).for_each(|key| {
-                hm.insert(key, $value);
-            });
-        )*
-
-        hm
-    }};
+           hm
+       }
+    }
 }
